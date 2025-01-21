e0a362c8e3959f27796f7ff7d7cf8ab38b99dedf
earocorn <amavb789@gmail.com>
2025-01-21 17:02:52 +0800

    foi and datastreams

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import Systems from 'osh-js/source/core/sweapi/system/Systems';
+import Observations from 'osh-js/source/core/sweapi/observation/Observations';
 import SystemFilter from "osh-js/source/core/sweapi/system/SystemFilter";
 import DataStreamFilter from "osh-js/source/core/sweapi/datastream/DataStreamFilter";
 import System from "osh-js/source/core/sweapi/system/System";
 import DataStreams from "osh-js/source/core/sweapi/datastream/DataStreams";
+import FeaturesOfInterest from "osh-js/source/core/sweapi/featureofinterest/FeatureOfInterests"
+import FeatureOfInterest from "osh-js/source/core/sweapi/featureofinterest/FeatureOfInterest"
 import VideoView from 'osh-js/source/core/ui/view/video/VideoView';
 import VideoDataLayer from 'osh-js/source/core/ui/layer/VideoDataLayer';
 import SweApi from "osh-js/source/core/datasource/sweapi/SweApi.datasource"
@@ -14,7 +17,7 @@ import "./App.css";
 function App() {
 
   const networkOpts = {
-    endpointUrl: `10.146.49.246:8282/sensorhub/api`,
+    endpointUrl: `localhost:8282/sensorhub/api`,
     tls: false,
     connectorOpts: {
         username: 'admin',
@@ -24,164 +27,46 @@ function App() {
 
   const systems = new Systems(networkOpts);
   const datastreams = new DataStreams(networkOpts);
+  const features = new FeaturesOfInterest(networkOpts);
+  const [obsArray, setObsArray] = useState<any[]>([]);
 
   useEffect(() => {
+    async function fetchFois() {
+      // console.log(features);
+      const allFoisCol = await features.searchFeaturesOfInterest(undefined, 99999);
+      const allFois = await allFoisCol.nextPage();
+      console.log("FOIs", allFois);
 
-    async function fetchSystems() {
-      // Get collection of top level systems by a tag (ex: "lane" to get systems with "Lane"/"lane" in the name) with ability to select pageSize in case of large amount of lanes
-      const availableSystemsCollection = await systems.searchSystems(new SystemFilter(), /*pageSize = */10);
-      const availableSystems = await availableSystemsCollection.nextPage();
-      console.log(availableSystems); // List of osh-js "System" objects
-      
-      const lane1 = availableSystems[0]; // Get whichever lane, filter by "properties.properties.uid" to ensure it matches "urn:osh:system:lane"
-      // console.log(lane1); 
-
-      const lane1SubsystemsCollection = await lane1.searchMembers(); // Returns collection of subsystems of the lane 
-      const lane1Subsystems = await lane1SubsystemsCollection.nextPage();
-      console.log(lane1Subsystems);
-
-      // Retrieve certain drivers from 
-      // const rpmDriver = lane1Subsystems.find((system: typeof System) => system.properties.properties.uid.startsWith("urn:osh:sensor:rapiscan"));
-
-      // Retrieve ALL Datastreams from a system
-      // const rpmDatastreamsCollection = await rpmDriver.searchDataStreams(undefined, /*pageSize = */30); // Get all datastreams 
-      // // Filter by observed property, this yields only the "Occupancy" Datastream
-      // const rpmOccupancyStreamsCollection = await rpmDriver.searchDataStreams(new DataStreamFilter({ observedProperty: ["http://www.opengis.net/def/occupancy"] }))
-      
-      // const occupancyDatastream = (await rpmOccupancyStreamsCollection.nextPage())[0];
-
-      // You can directly subscribe to a datastreams observations
-      // occupancyDatastream.streamObservations(undefined, (message: any) => {
-      //   console.log(message);
-      // });
-
-      // Or create a SweApi for videostream observations
-      // const process = availableSystems.find((system: typeof System) => system.properties.properties.uid.includes("urn:osh:sensor:"));
-      // console.log("Process: ")
-      // console.log(process)
-      // const processDatastreamsCol = await process.searchDataStreams(undefined, 50);
-      // const processDatastreams = await processDatastreamsCol.nextPage();
-      // console.log("Process datastreams: ")
-      // console.log(processDatastreams)
-      // const videoDatastreamsCol = await videoDriver.searchDataStreams(new DataStreamFilter({ ObservationFilter: ["http://www.opengis.net/def/Video"] }));
-
-      // Get all datastreams from a node paginated for whatever size
-      if(datastreams !== undefined) {
-        const allDatastreamsCol = await datastreams.searchDataStreams(new DataStreamFilter({ 
-          q: "urn:osh:sensor:ffmpeg:alexvideocam:video",
-        }), /*pageSize = */100);
-        const allDatastreams = await allDatastreamsCol.nextPage();
-        const filteredDatastreams = allDatastreams.filter((ds: any) => ds.properties.observedProperties[0].definition.includes("http://sensorml.com/ont/swe/property/RasterImage")
-      && ds.properties.outputName === "urn:osh:sensor:ffmpeg:alexvideocam:video");
-        console.log(filteredDatastreams); // Prints 86 individual datastreams that are directly subscribable
+      for (let index = 0; index < 30; index++) {
+        // fetchDataStreamsFromFoi(allFois[index]);
+        fetchSystemsFromFoi(allFois[index]);
       }
     }
 
-    fetchSystems();
+    async function fetchDataStreamsFromFoi(foi: typeof FeatureOfInterest) {
+        const dsCol = await datastreams.searchDataStreams(new DataStreamFilter({ foi: foi.properties.id }), 1000);
+        const ds = await dsCol.nextPage();
+
+        console.log(`FOI ${foi.properties.id} datastreams:`, ds);
+    }
+
+    async function fetchSystemsFromFoi(foi: typeof FeatureOfInterest) {
+      const sysCol = await systems.searchSystems(new SystemFilter({ foi: foi.properties.id }), 1000);
+      const sys = await sysCol.nextPage();
+
+      console.log(`FOI ${foi.properties.id} systems:`, sys);
+  }
+
+    fetchFois();
 
     }
   , []);
 
-  let masterTimeController: typeof DataSynchronizer = null;
-  let videoDs: typeof SweApi = null;
-
-  useEffect(() => {
-    videoDs = new SweApi("Videostream", {
-        connectorOpts: networkOpts.connectorOpts,
-        endpointUrl: networkOpts.endpointUrl,
-        tls: networkOpts.tls, 
-        resource: "/datastreams/20b4tbiv3i04s/observations", // 20b4tbiv3i04s // jh6sinh0g3oos
-        mode: "replay",
-        protocol: "ws",
-        responseFormat: "application/swe+binary",
-        startTime: "2024-11-18T19:21:40Z",
-        endTime: "2024-11-18T19:21:53Z"
-      });
-      // let video2Ds: any = useMemo(() => 
-      //   new SweApi("Videostream", {
-      //     connectorOpts: networkOpts.connectorOpts,
-      //     endpointUrl: networkOpts.endpointUrl,
-      //     tls: networkOpts.tls, 
-      //     resource: "/datastreams/jh6sinh0g3oos/observations", // 20b4tbiv3i04s // jh6sinh0g3oos
-      //     mode: "replay",
-      //     protocol: "ws",
-      //     responseFormat: "application/swe+binary",
-      //     startTime: "2024-11-18T19:21:40Z",
-      //     endTime: "2024-11-18T19:21:53Z"
-      //   }), []);
-
-    masterTimeController = new DataSynchronizer({
-      startTime: "2024-11-18T19:21:40Z",
-      endTime: "2024-11-18T19:21:53Z",
-      replaySpeed: 1,
-      intervalRate: 5,
-      dataSources: [videoDs]
-    });
-
-      let videoView = new VideoView({
-        container: "video-container",
-        name: "cat video",
-        showTime: false,
-        showStats: false,
-        layers: [new VideoDataLayer({
-          dataSourceId: [videoDs.getId()],
-          getFrameData: (rec: any) => {
-            return rec.img;
-          },
-          getTimestamp: (rec:any) => {
-            return rec.timestamp;
-          }
-        })]
-      });
-      
-      // let video2View = useMemo(() => new VideoView({
-      //   container: "video2-container",
-      //   name: "cat video2",
-      //   showTime: false,
-      //   showStats: false,
-      //   layers: [new VideoDataLayer({
-      //     dataSourceId: [videoDs.getId()],
-      //     getFrameData: (rec: any) => {
-      //       return rec.img;
-      //     },
-      //     getTimestamp: (rec:any) => {
-      //       return rec.timestamp;
-      //     }
-      //   })]
-      // }), [video2Ds]);
-  }, []);
-
-  useEffect(() => {
-    masterTimeController.connect();
-  }, [videoDs]);
-
-    // useEffect(() => {
-    //   console.log(videoDs)
-    //   async function tryConnect() {
-    //     if(videoDs === undefined || videoDs === null) {
-    //       return;
-    //     }
-    //     const isConnected = await videoDs.isConnected();
-    //     if(!isConnected) {
-    //       console.log("Connecting")
-    //       // videoDs.connect();
-    //       masterTimeController.connect();
-    //     }
-    //   }
-    //   tryConnect();
-    // }, [videoDs]);
-
   return (
     <div className="App" style={{ flex: 1, flexDirection: 'row'}}>
-      <h1>Lane: dd</h1>
-      <div style={{ padding: 50 }}>
-          <h2>Video From Livestream</h2>
-          <div id="video-container" style={{ width: "100%", height: "100%" }}/>
-      </div>
-      <div style={{ padding: 50 }}>
-          <h2>Video From Process Data</h2>
-          <div id="video2-container" style={{ width: "100%", height: "100%" }}/>
-      </div>
+      {/* {obsArray.map((obs, index) => (
+        <img key={index} src={obs.result["即時影像"]} alt={`Data from observation ${obs.id}`} />
+      ))} */}
     </div>
   );
 }
