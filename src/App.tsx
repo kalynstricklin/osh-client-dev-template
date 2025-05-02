4dfedb69cfa0f6a76d9ec35b7e09445528251d52
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2025-05-02 13:57:19 -0500

    h264 video

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SamplingFeatures from "osh-js/source/core/consysapi/samplingfeature/Sampl
 import Observations from "osh-js/source/core/consysapi/observation/Observations.js"
 import ObservationFilter from "osh-js/source/core/consysapi/observation/ObservationFilter.js"
 import VideoView from "osh-js/source/core/ui/view/video/VideoView.js"
+import WebCodecView from "osh-js/source/core/ui/view/video/WebCodecView.js"
 import VideoDataLayer from "osh-js/source/core/ui/layer/VideoDataLayer.js"
 import Commands from "osh-js/source/core/consysapi/command/Commands.js"
 import ConSysApi from "osh-js/source/core/datasource/consysapi/ConSysApi.datasource.js"
@@ -18,7 +19,7 @@ import "./App.css";
 function App() {
 
   const networkOpts ={
-    endpointUrl: '104.179.198.202:8282/sensorhub/api',
+    endpointUrl: 'localhost:8282/sensorhub/api',
     connectorOpts: {
         username: 'admin',
         password: 'oscar',
@@ -28,7 +29,7 @@ function App() {
   const systems = new Systems(networkOpts);
   const datastreams = new DataStreams(networkOpts);
   const observations = new Observations(networkOpts);
-  const controlstreams = new ControlStreams(networkOpts);
+  const controlStreams = new ControlStreams(networkOpts);
   const commands = new Commands(networkOpts);
   const features = new SamplingFeatures(networkOpts);
 
@@ -40,131 +41,135 @@ function App() {
   let ds: typeof DataStream = null;
   let masterTimeController: typeof DataSynchronizer = null;
 
-  const [weatherObs, setWeatherObs] = useState("");
-
   useEffect(() => {
     async function fetchData() {
       // Lane system
-      const sys = await systems.getSystemById("haso9uhmu6imu");
+      // const sys = await systems.getSystemById("h2rb9h4lli260");
+      const sys = await systems.getSystemById("fcmsn2pgmtp5i");
       console.log(sys);
 
-      ds = await datastreams.getDataStreamById("mkin73ik1q45m");
+      // ds = await datastreams.getDataStreamById("ucq0mc4cdpbqa");
+      ds = await datastreams.getDataStreamById("njson8jtrdm4i");
 
       videoSource = new ConSysApi('video', {
         protocol: ds.networkProperties.streamProtocol,
         endpointUrl: ds.networkProperties.endpointUrl,
         resource: `/datastreams/${ds.properties.id}/observations`,
         tls: false,
-        startTime: "2025-04-23T16:51:53Z",
-        endTime: "2025-04-23T17:54:32Z",
+        startTime: "2025-05-02T18:25:38.000Z",
+        endTime: "2025-05-02T18:28:43.000Z",
         responseFormat: "application/swe+binary",
         mode: Mode.REPLAY,
       });
 
       console.log(ds);
 
-      // weatherDs.streamObservations(undefined, (obs: any) => {
-      //   setWeatherObs(obs[0].result);
-      //   // console.log(obs[0].result);
-      // });
-
       videoLayer = new VideoDataLayer({
         dataSourceId: videoSource.id,
         getFrameData: (rec: any) => rec.img,
         getTimestamp: (rec: any) => rec.timestamp,
+        // getRoll: (rec: any) => rec.roll,
+
       })
 
       videoView = new VideoView({
-        container: "video-view",
-        showStats: false,
+        container: "video-h264-container",
         showTime: false,
-        layers: [videoLayer]
+        showStats: false,
+        useWebCodecApi: true,
+        layers: [videoLayer],
+
       });
 
       masterTimeController =  new DataSynchronizer({
         replaySpeed: 1,
         intervalRate: 5,
-        dataSources: [videoSource]
+        dataSources: [videoSource],
+        // startTime: "2025-05-02T18:25:38.000Z",
+        // endTime: "2025-05-02T18:28:43.000Z",
       });
+
+      await masterTimeController.connect();
     }
     fetchData();
   }, []);
 
-  function start() {
-    masterTimeController.connect();
-  }
-
-  function moveTime(newTime: String) {
-    console.log(masterTimeController);
-    masterTimeController.setTimeRange(
-        newTime,
-        "2025-04-23T17:52:32Z",
-        masterTimeController.getReplaySpeed(),
-        false
-    );
-  }
-
-  function printCurrentData() {
-    console.log(videoLayer.getProps())
-    console.log(videoLayer.getCurrentProps())
-  }
-  async function setFrame() {
-
-    const sys = await systems.getSystemById("haso9uhmu6imu");
-
-    const ds = await datastreams.getDataStreamById("mkin73ik1q45m");
-
-    const obs = await ds.searchObservations(new ObservationFilter({
-      format: 'application/swe+binary',
-      phenomenonTime: `${/*startTime+500ms*/}/${/*endTime-500ms*/}`
-    }), 1);
-
-    const obsPage = await obs.nextPage();
-
-    let imgBlob = new Blob([obsPage[3].img.data]);
-    let url = window.URL.createObjectURL(imgBlob);
-
-    var imgTag = document.getElementById("test");
-    let oldBlobURL = imgTag.src;
-    imgTag.src = url;
-  }
-
-  let frameSrc = "";
-
-  function pause() {
-    console.log(masterTimeController)
-    masterTimeController.disconnect();
-    console.log(masterTimeController)
-    var img = document.getElementsByClassName("video-mjpeg");
-    frameSrc = (img[0].src);
-  }
-
-  function play() {
-    console.log(masterTimeController);
-    var img = document.getElementsByClassName("video-mjpeg");
-
-    masterTimeController.connect().finally(() => {
-      if(img.length > 0) {
-        console.log("Setting src to ", img[0].src);
-        img[0].src = frameSrc;
-        console.log(frameSrc);
-      }
-    });
-  }
+  // function start() {
+  //   masterTimeController.connect();
+  // }
+  //
+  // function moveTime(newTime: String) {
+  //   console.log(masterTimeController);
+  //   masterTimeController.setTimeRange(
+  //       newTime,
+  //       "2025-05-02T14:28:57Z",
+  //       masterTimeController.getReplaySpeed(),
+  //       false
+  //   );
+  // }
+  //
+  // function printCurrentData() {
+  //   console.log(videoLayer.getProps())
+  //   console.log(videoLayer.getCurrentProps())
+  // }
+  //
+  // async function setFrame() {
+  //
+  //   const sys = await systems.getSystemById("fcmsn2pgmtp5i");
+  //
+  //   const ds = await datastreams.getDataStreamById("njson8jtrdm4i");
+  //
+  //   const obs = await ds.searchObservations(new ObservationFilter({
+  //     format: 'application/swe+binary',
+  //     // phenomenonTime: `${/*startTime+500ms*/}/${/*endTime-500ms*/}`
+  //   }), 1);
+  //
+  //   const obsPage = await obs.nextPage();
+  //
+  //   // let imgBlob = new Blob([obsPage[3].img.data]);
+  //   // let url = window.URL.createObjectURL(imgBlob);
+  //
+  //   // var imgTag = document.getElementById("test");
+  //   // let oldBlobURL = imgTag.src;
+  //   // imgTag.src = url;
+  // }
+  //
+  // let frameSrc = "";
+  //
+  // function pause() {
+  //   console.log(masterTimeController)
+  //   masterTimeController.disconnect();
+  //   console.log(masterTimeController)
+  //   // var img = document.getElementsByClassName("video-mjpeg");
+  //   // frameSrc = (img[0].src);
+  // }
+  //
+  // function play() {
+  //   console.log(masterTimeController);
+  //   // var img = document.getElementsByClassName("video-mjpeg");
+  //
+  //   masterTimeController.connect();
+  //       // .finally(() => {
+  //     // if(img.length > 0) {
+  //       // console.log("Setting src to ", img[0].src);
+  //       // img[0].src = frameSrc;
+  //       // console.log(frameSrc);
+  //     // }
+  //   // });
+  // }
 
   return (
     <div className="App" style={{ flex: 1, flexDirection: 'row'}}>
-      <h2>Streaming data from weather sensor</h2>
-      {/*<div>{JSON.stringify(weatherObs)}</div>*/}
-      {/*<div id='rainChart'>rain</div>*/}
-      <button onClick={() => play()}>START</button>
-      <button onClick={() => pause()}>PAUSE</button>
-      <button onClick={() => setFrame()}>SET FRAME</button>
-      <button onClick={() => moveTime(time)}>MOVE TIME</button>
-      <button onClick={() => printCurrentData()}>CURRENT DATA</button>
+      <h2>Streaming data from H264 Videos</h2>
+      {/*<button onClick={() => start()}>START</button>*/}
+      {/*<button onClick={() => play()}>PLAY</button>*/}
+      {/*<button onClick={() => pause()}>PAUSE</button>*/}
+      {/*<button onClick={() => setFrame()}>SET FRAME</button>*/}
+      {/*<button onClick={() => moveTime(time)}>MOVE TIME</button>*/}
+      {/*<button onClick={() => printCurrentData()}>CURRENT DATA</button>*/}
       <input type="text" value={time} onChange={e => setTime(e.target.value)}></input>
-      <div id='video-view' style={{ height:"100%", width:"100%"}}></div>
-      <img id='test'></img>
+      <div id='video-h264-container' style={{ height:"300px", width:"500px"}}></div>
+      {/*<img id='test'></img>*/}
     </div>
     
   );
